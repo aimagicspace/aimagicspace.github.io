88299e9c71cd2b12f85c5e6974000dba1e83c3b0
AI Magic Space <120978933+aimagicspace@users.noreply.github.com>
2023-01-22 23:16:24 +0800

    Add files via upload

---
+++ arthouse/files/js/country.js
@@ -0,0 +1,17 @@
+$.get("https://ipinfo.io", function (response) {
+    console.log(response.country);
+	if (response.country == "MY") {
+	
+		$(".my").show();
+		$(".us").hide();
+		//$(".us").show();
+		//$(".my").hide();
+	}
+	else {
+		$(".us").show();
+		$(".my").hide();
+	}
+    //$("#country").html("Country: " + response.country);
+    //$("#address").html("Location: " + response.city + ", " + response.region);
+    //$("#details").html(JSON.stringify(response, null, 4));
+}, "jsonp");
